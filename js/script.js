f69a1f6842dff304ca6666497c90504e58333397
kt-std <kt.std2020@gmail.com>
2021-02-26 22:30:32 +0200

    Disable booking button

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
-document.getElementById('bookingForm').addEventListener('change', (e)=>{
+document.getElementById('bookingForm').addEventListener('change', (e) => {
     const checkedSeats = document.querySelectorAll('.seat:checked'),
         checkedDate = document.querySelector('input[name="date"]:checked'),
         checkedTime = document.querySelector('input[name="time"]:checked'),
         checkedHall = document.querySelector('input[name="type_picker"]:checked');
-    
-    updateElementInnerHTML(getListOfCheckedSeats(checkedSeats), 'seatsContainer');
-    appendInfoToContainer(checkedDate, 'ticketDate');
-    appendInfoToContainer(checkedTime, 'ticketTime');
-    appendInfoToContainer(checkedHall, 'hallContainer');
+    if (checkedSeats.length) {
+        document.getElementById('submit').disabled = false;
+        updateElementInnerHTML(getListOfCheckedSeats(checkedSeats), 'seatsContainer');
+        appendInfoToContainer(checkedDate, 'ticketDate');
+        appendInfoToContainer(checkedTime, 'ticketTime');
+        appendInfoToContainer(checkedHall, 'hallContainer');
+    }else{
+        document.getElementById('submit').disabled = true;
+        cleanTicket();
+    }   
 });
 
-function appendInfoToContainer(input, containerId){
+function cleanTicket(ticket) {
+    document.querySelectorAll('.ticket__description').forEach(ticketField => ticketField.innerHTML = "");
+}
+
+function appendInfoToContainer(input, containerId) {
     document.getElementById(containerId).innerHTML = input.value;
 }
 
-function updateElementInnerHTML(info, containerId){
+function updateElementInnerHTML(info, containerId) {
     document.getElementById(containerId).innerHTML = info;
 }
 
-function getListOfCheckedSeats(checkedSeats){
+function getListOfCheckedSeats(checkedSeats) {
     return Array.from(checkedSeats).reduce((accumulator, seat) => accumulator += getFormattedSeatNumber(seat.value), '');
 }
 
-function getFormattedSeatNumber(seat){
+function getFormattedSeatNumber(seat) {
     const [seatRow, seatNumber] = seat.split('_');
     return `R${seatRow}S${seatNumber}<br>`;
 }
+
+window.addEventListener('beforeunload', (event) => {
+    document.getElementById('bookingForm').reset()
+});
